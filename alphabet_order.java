df7eba7db42f9aa0aa76186e22d1c8257a853e4a
ko1aru <83403149+ko1aru@users.noreply.github.com>
2021-10-29 12:37:27 +0530

    Add files via upload

---
+++ alphabet_order.java
@@ -0,0 +1,14 @@
+public class alphabet_order {
+
+	public static void main(String[] args) {
+		int i,j;
+		for (i=1; i<=5; i++) {
+			for (j=1; j<=i; j++) {
+				System.out.print((char)(j+64));
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
---
+++ zero_one_pattern.java
@@ -0,0 +1,19 @@
+
+public class zero_one_pattern {
+
+	public static void main(String[] args) {
+		int i,j;
+		for (i=1;i<=5;i++) {
+			for (j=1; j<=i; j++) {
+				if (j%2==0) {
+					System.out.print(0);
+				} else {
+					System.out.print(1);
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
